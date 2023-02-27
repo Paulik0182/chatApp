393e4d3a2034c43f64908792624935baea484482
Paul <paulik0182@gmail.com>
2023-02-27 17:07:32 +0300

    Исправление разметки (заменен item, коррекция поля для ввода сообщения)

--- app/src/main/java/com/paulik/chatapp/ui/details/DetailsChatViewHolder.kt
+++ app/src/main/java/com/paulik/chatapp/ui/details/DetailsChatViewHolder.kt
@@ -5,24 +5,23 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.paulik.chatapp.R
-import com.paulik.chatapp.databinding.ItemRecordMessagesBinding
+import com.paulik.chatapp.databinding.ItemMessagesNotMyBinding
 import com.paulik.chatapp.domain.entity.MessageEntity
 
 class DetailsChatViewHolder(
     parent: ViewGroup,
     val context: Context,
 ) : RecyclerView.ViewHolder(
-    LayoutInflater.from(parent.context).inflate(R.layout.item_record_messages, parent, false)
+    LayoutInflater.from(parent.context).inflate(R.layout.item_messages_not_my, parent, false)
 ) {
 
-    private val binding: ItemRecordMessagesBinding = ItemRecordMessagesBinding.bind(itemView)
+    private val binding: ItemMessagesNotMyBinding = ItemMessagesNotMyBinding.bind(itemView)
     private lateinit var messageEntity: MessageEntity
 
     fun bind(messageEntity: MessageEntity) {
         this.messageEntity = messageEntity
 
         binding.textMessageReceivedTextView.text = messageEntity.text
-        binding.textMessageSentTextView.text = messageEntity.text
     }
 
 }
